dbd5f2dea7ac5e252d88b345a7b47cd5ac1b3df8
JakubSchwenkbeck <jakub@schwenkbeck.de>
2024-08-09 17:39:23 +0200

    restructured code

--- Plot.py
+++ Plot.py
@@ -8,8 +8,18 @@ plt.subplots_adjust(bottom=0.2)  # Adjust the plot to make room for the button
 plot1, = ax.plot(sigma, np.abs(zeta_values), label=r'$|ζ(σ+it)|$')
 ax.axhline(0, color='black', linewidth=0.5)
 AXVLINE = ax.axvline(1, color='red', linestyle='--', label='Critical Line $σ=1$')
-# still problems with switcher
 
+global plotstate
+plotstate = 0
+
+
+
+DICT_PLOTSTATE = {
+    'PRIM_APPROX' : 0,
+    'ZETA_STANDARD' : 1
+}
+
+ 
 
 ax.set_xlabel(r'$σ$')
 ax.set_ylabel(r'$|ζ(σ+it)|$')
@@ -18,28 +28,47 @@ ax.legend()
 ax.grid(True)
 
 
+
+def show_prim_approx():
+    5
+
+
+
+
+# Show standrad zeta function 
 def show_standard_zeta():
     plot1.set_xdata(sigma)
     plot1.set_ydata( np.abs(zeta_values))
-    ax.set_xlabel(r'Magnitude of the Riemann Zeta Function for $t=14.134725$')
-    ax.set_ylabel( r'$σ$')
-    ax.set_title(r'$|ζ(σ+it)|$')
+    ax.set_xlabel(r'$σ$')
+    ax.set_ylabel(r'$|ζ(σ+it)|$')
+    ax.set_title(r'Magnitude of the Riemann Zeta Function for $t=14.134725$')
     AXVLINE.set_visible(True)
+    
+    
+    
+
+    
+    
 # Function to switch plots
 def switch(event):
-    if plot1.get_xdata().all() == sigma.all():
-        # placeholder:
+    global plotstate
+    
+    if plotstate == DICT_PLOTSTATE['ZETA_STANDARD']:
+ # Switch back to the original zeta plot
+        show_standard_zeta()   
+        plotstate = plotstate+1
+    else:
+       
+     # placeholder:
         # Switch to sine plot
         plot1.set_xdata(x)
         plot1.set_ydata(y2)
         ax.set_xlabel('x')
         ax.set_ylabel('sin(x)')
         ax.set_title('Sine Wave')
-        AXVLINE.set_visible(False) #ax.axvline(1, color='black', linestyle='--', label='Critical Line $σ=1$') # Hide the critical line
-    else:
-        # Switch back to the original zeta plot
-        show_standard_zeta()
-        
+        AXVLINE.set_label = ' '
+        AXVLINE.set_visible(False) # Hide the critical line    
+        plotstate = plotstate-1
         
     ax.legend()
     plt.draw()  # Redraw the plot
