1c48f9cd50053d93c85ce5f811757a44b4e84205
JakubSchwenkbeck <jakub@schwenkbeck.de>
2024-08-11 16:46:01 +0200

    rewrote the 2d zeta calculation

--- Riemann_Zeta.py
+++ Riemann_Zeta.py
@@ -61,11 +61,6 @@ def standard_riemann_zeta(s, n_terms=1000):
     return sum(1/n**s for n in range(1, n_terms + 1))
 
 
-sigma = np.linspace(0.5, 1.5, 400)
-t = 14.134725
-s = sigma + t * 1j
-
-zeta_values = np.array([standard_riemann_zeta(sigma + t * 1j) for sigma in sigma])
 
 
 
